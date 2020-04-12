4d603eb5cd07cbb928bec6622302bcc2a345f4e1
Alongkot Intaragumhang <alonkkot.non@gmail.com>
2020-04-12 14:31:10 +0700

    test2

--- eclipse-workspace/FinalProject_hub99/src/Ball.java
+++ eclipse-workspace/FinalProject_hub99/src/Ball.java
@@ -1,4 +1,39 @@
 
-public class Ball {
+public abstract class Ball {
+	final float radius = 20; //to be edited
+	protected float x_coordinate;
+	protected float y_coordinate;
+	protected float speed;
+	
+	public Ball(float x, float y, float speed) {
+		this.setX_coordinate(x);
+		this.setY_coordinate(y);
+		this.setSpeed(speed);
+	}
+	
+	public abstract void bounce();
 
+	public float getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(float speed) {
+		this.speed = speed;
+	}
+
+	public float getX_coordinate() {
+		return x_coordinate;
+	}
+
+	public void setX_coordinate(float x_coordinate) {
+		this.x_coordinate = x_coordinate;
+	}
+
+	public float getY_coordinate() {
+		return y_coordinate;
+	}
+
+	public void setY_coordinate(float y_coordinate) {
+		this.y_coordinate = y_coordinate;
+	}
 }
